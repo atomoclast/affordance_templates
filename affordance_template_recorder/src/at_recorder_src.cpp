eac68cf278b5a09422c99cae8b236414f106f621
karan <karan@traclabs.com>
2015-05-01 19:32:51 -0500

    Karan - added code to determine the transform to be stored in the JSON file, with deletable comments. Numbers not adding

--- affordance_template_recorder/src/at_recorder_src.cpp
+++ affordance_template_recorder/src/at_recorder_src.cpp
@@ -6,6 +6,7 @@
 //#include <tf/tfScalar.h>
 #include <iostream>
 #include <fstream>
+#include <kdl/frames.hpp>
 
 int main(int argc, char** argv)
 {
@@ -26,17 +27,22 @@ int main(int argc, char** argv)
   std::ofstream op_file_id;
   op_file_id.open("op_file.txt");
 
+  KDL::Frame T_ee_obj, T_abs_obj, T_abs_ee;
+  KDL::Frame T_ee_abs;
+  //T_ee_obj = T_abs_obj * T_ee_abs
+  //T_abs_obj = T_ee_obj * inv(T_ee_abs)
+  
   //std::cout << base_frame << "\t" << target_frame << std::endl;
   
   ros::init(argc, argv, "record_traj_point_node");
   
   ros::NodeHandle node;
 
-  ros::Rate rate(0.5);
+  ros::Rate rate(0.1);
  
   tf::StampedTransform transform;
   geometry_msgs::Vector3 position;
-  tf::Matrix3x3 matrix;
+  tf::Matrix3x3 tf_matrix;
   double roll, pitch, yaw;
 
   tf::TransformListener listener;
@@ -58,15 +64,62 @@ int main(int argc, char** argv)
     position.y = transform.getOrigin().y();
     position.z = transform.getOrigin().z();
 
-    matrix.setRotation(transform.getRotation());
-    matrix.getRPY(roll, pitch, yaw);
-    //matrix.getEulerYPR(yaw, pitch, roll);
+    tf_matrix.setRotation(transform.getRotation());
+    tf_matrix.getRPY(roll, pitch, yaw);
+    tf_matrix.getEulerYPR(yaw, pitch, roll);
+
+    //Populate the translation elements of T_abs_obj
+
+    T_ee_obj.p.data[0] = position.x;
+    T_ee_obj.p.data[1] = position.y;
+    T_ee_obj.p.data[2] = position.z;
+
+    //Populate the rotation elements of T_abs_obj
+
+    /*tf::Vector3 tf_matrix_row[3];
+    tf_matrix_row[0] = tf_matrix.getRow(0);
+    tf_matrix_row[1] = tf_matrix.getRow(1);
+    tf_matrix_row[2] = tf_matrix.getRow(2);
+ 
+    T_ee_obj.M.data[0] = tf_matrix_row[0].getX();	T_ee_obj.M.data[1] = tf_matrix_row[0].getY();	T_ee_obj.M.data[2] = tf_matrix_row[0].getZ();
+    T_ee_obj.M.data[3] = tf_matrix_row[1].getX();	T_ee_obj.M.data[4] = tf_matrix_row[1].getY();	T_ee_obj.M.data[5] = tf_matrix_row[1].getZ();
+    T_ee_obj.M.data[6] = tf_matrix_row[2].getX();	T_ee_obj.M.data[7] = tf_matrix_row[2].getY();	T_ee_obj.M.data[8] = tf_matrix_row[2].getZ();*/
+
+    T_ee_obj.M.RPY(roll, pitch, yaw);
+
     
     std::cout << position.x << "\t" << position.y << "\t" << position.z << "\t" << std::endl;
-    std::cout << roll << "\t" << pitch << "\t" << yaw << "\t" << std::endl;
+    /*std::cout << roll << "\t" << pitch << "\t" << yaw << "\t" << std::endl;
     
     op_file_id << position.x << "\t" << position.y << "\t" << position.z << "\n";
-    op_file_id << roll << "\t" << pitch << "\t" << yaw << "\n\n";
+    op_file_id << roll << "\t" << pitch << "\t" << yaw << "\n\n";*/
+
+    //Populate the translation elements of T_abs_ee
+
+    T_ee_abs.p.data[0] = 0.1;
+    T_ee_abs.p.data[1] = -0.2;
+    T_ee_abs.p.data[2] = 0.0;
+
+    //Populate the rotation elements of T_abs_ee 
+
+    /*tf_matrix.setRPY(0.0, 1.57, 3.14);
+
+    tf_matrix_row[0] = tf_matrix.getRow(0);
+    tf_matrix_row[1] = tf_matrix.getRow(1);
+    tf_matrix_row[2] = tf_matrix.getRow(2);
+
+    T_ee_abs.M.data[0] = tf_matrix_row[0].getX();	T_ee_abs.M.data[1] = tf_matrix_row[0].getY();	T_ee_abs.M.data[2] = tf_matrix_row[0].getZ();
+    T_ee_abs.M.data[3] = tf_matrix_row[1].getX();	T_ee_abs.M.data[4] = tf_matrix_row[1].getY();	T_ee_abs.M.data[5] = tf_matrix_row[1].getZ();
+    T_ee_abs.M.data[6] = tf_matrix_row[2].getX();	T_ee_abs.M.data[7] = tf_matrix_row[2].getY();	T_ee_abs.M.data[8] = tf_matrix_row[2].getZ();*/
+
+    T_ee_abs.M.RPY(0.0, 1.57, 3.14);
+
+    T_abs_obj = T_ee_obj *  T_ee_abs.Inverse();
+
+    std::cout << T_ee_obj.M.data[0] << "\t" <<	T_ee_obj.M.data[1] << "\t" <<	T_ee_obj.M.data[2] << "\t" << T_ee_obj.p.data[0] << std::endl;
+    std::cout << T_ee_obj.M.data[3] << "\t" <<	T_ee_obj.M.data[4] << "\t" <<	T_ee_obj.M.data[5] << "\t" << T_ee_obj.p.data[1] << std::endl;
+    std::cout << T_ee_obj.M.data[6] << "\t" <<	T_ee_obj.M.data[7] << "\t" <<	T_ee_obj.M.data[8] << "\t" << T_ee_obj.p.data[2] << std::endl;
+    std::cout << std::endl;
 
     rate.sleep();
 
