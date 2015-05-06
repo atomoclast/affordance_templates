a5b73df0af4891c48f8e2c6ae382b3d60d60586a
Stephen Hart <swhart@traclabs.com>
2015-05-06 11:51:26 -0500

    made at recorder script work added appropriate rosbuild/catkin config files

--- affordance_template_recorder/src/at_recorder_src.cpp
+++ affordance_template_recorder/src/at_recorder_src.cpp
@@ -14,21 +14,28 @@ int main(int argc, char** argv)
   if (argc!=3)
   {
     std::cout << "Three arguments are needed" << std::endl;
-    std::cout << "When running exe, the format should be \"exe <base-frame> <target-frame>\"" << std::endl;
-    std::cout << "e.g. ./at_recorder Valve:0 r_default_tool" << std::endl;
+    std::cout << "./at_recorder [obj_frame] [right|left]" << std::endl;
     std::cout << "Quitting!" << std::endl;
 
     exit(1);
   }
   
   std::string base_frame = argv[1];
-  std::string target_frame = argv[2];
+  std::string hand = argv[2];
+  std::string target_frame = "";
 
+  if(hand=="right") {
+    target_frame = "r_end";
+  } else {
+    target_frame = "l_end";
+  }
+  
   std::ofstream op_file_id;
   op_file_id.open("op_file.txt");
 
   KDL::Frame T_ee_obj, T_abs_obj, T_abs_ee;
   KDL::Frame T_ee_abs;
+  KDL::Frame T_viz_offset;
   //T_ee_obj = T_abs_obj * T_ee_abs
   //T_abs_obj = T_ee_obj * inv(T_ee_abs)
   
@@ -83,18 +90,34 @@ int main(int argc, char** argv)
     op_file_id << roll << "\t" << pitch << "\t" << yaw << "\n\n";*/
 
     //Populate the translation elements of T_ee_abs
-    T_ee_abs.p.data[0] = 0.1;
-    T_ee_abs.p.data[1] = -0.2;
-    T_ee_abs.p.data[2] = 0.0;
-
-    //Populate the rotation elements of T_ee_abs
-    T_ee_abs.M = KDL::Rotation::RPY(0.0, 1.57, 3.14);
-
+    if(hand=="right") {
+      T_ee_abs.p.data[0] = 0.1;
+      T_ee_abs.p.data[1] = -0.2;
+      T_ee_abs.p.data[2] = 0.0;
+      T_ee_abs.M = KDL::Rotation::RPY(0.0, 1.57, 3.14);
+    } else {
+      T_ee_abs.p.data[0] = 0.1;
+      T_ee_abs.p.data[1] = 0.2;
+      T_ee_abs.p.data[2] = 0.0;
+      T_ee_abs.M = KDL::Rotation::RPY(0.0, 1.57, 3.14);
+    }
     std::cout << "yaml: hand wrt abstract" << std::endl;
     std::cout << "position: " << T_ee_abs.p.data[0] << "\t" << T_ee_abs.p.data[1] << "\t" << T_ee_abs.p.data[2] << "\t" << std::endl;
     std::cout << "rotation: " << 0.0 << "\t" << 1.57 << "\t" << 3.14 << "\t" << std::endl << std::endl;
 
-    T_abs_obj = T_ee_obj *  T_ee_abs.Inverse();
+
+    if(hand=="right") {
+      T_viz_offset.p.data[0] = 0.0;
+      T_viz_offset.p.data[1] = -0.1;
+      T_viz_offset.p.data[2] = 0.0;
+      T_viz_offset.M = KDL::Rotation::RPY(0.0, 0.0, -1.57);
+    } else {
+      T_viz_offset.p.data[0] = 0.0;
+      T_viz_offset.p.data[1] = 0.1;
+      T_viz_offset.p.data[2] = 0.0;
+      T_viz_offset.M = KDL::Rotation::RPY(0.0, 0.0, 1.57);
+    }
+    T_abs_obj = T_ee_obj * T_viz_offset.Inverse() * T_ee_abs.Inverse();
 
     /*std::cout << "T_abs_obj" << std::endl;
     std::cout << T_abs_obj.M.data[0] << "\t" <<	T_abs_obj.M.data[1] << "\t" <<	T_abs_obj.M.data[2] << "\t" << T_abs_obj.p.data[0] << std::endl;
@@ -105,8 +128,8 @@ int main(int argc, char** argv)
     T_abs_obj.M.GetRPY(roll, pitch, yaw);
 
     std::cout << "abstract wrt object" << std::endl;
-    std::cout << "position: " << T_abs_obj.p.data[0] << "\t" << T_abs_obj.p.data[1] << "\t" << T_abs_obj.p.data[2] << "\t" << std::endl;
-    std::cout << "rotation: " << roll << "\t" << pitch << "\t" << yaw << "\t" << std::endl << std::endl;
+    std::cout << "\"xyz\": [" << T_abs_obj.p.data[0] << ",  " << T_abs_obj.p.data[1] << ",  " << T_abs_obj.p.data[2] << "]," << std::endl;
+    std::cout << "\"rpy\": [" << roll << ",  " << pitch << ",  " << yaw << "]" << std::endl << std::endl;
 
     rate.sleep();
 
