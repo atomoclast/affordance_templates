a129aa4e7569240f41755b7ff33ad98cfec28e74
Seth G <seth@traclabs.com>
2015-10-14 13:00:22 -0500

    part of server (re)rewritten; builds

--- affordance_template_server/include/affordance_template_server/server.h
+++ affordance_template_server/include/affordance_template_server/server.h
@@ -1,13 +1,18 @@
-#ifndef AT_SERVER_H_
-#define AT_SERVER_H_
+#ifndef _AT_SERVER_H_
+#define _AT_SERVER_H_
 
 #include <iostream>
+#include <boost/filesystem.hpp>
 
 #include <rapidjson/document.h>
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
 
 #include <ros/ros.h>
+#include <ros/package.h>
+#include <tf/transform_listener.h>
+#include <affordance_template_msgs/RobotConfig.h>
+#include <affordance_template_markers/robot_interface.h>
 
 using namespace rapidjson;
 
@@ -15,9 +20,26 @@ namespace affordance_template_server
 {
   class AffordanceTemplateServer
   {
+    void run();
+    std::map<std::string, affordance_template_markers::RobotInterface&> getAvailableRobots(const std::string&);
+    inline bool getStatus() { return status_; }
+
+    tf::TransformListener listener_;
+
+    bool status_;
+  
   public:
     AffordanceTemplateServer();
     ~AffordanceTemplateServer();
+    
+    // void configureServer();
+    // bool addTemplate();
+    // bool removeTemplate();
+    // int getNextTemplateId();
+    std::string getPackagePath(const std::string&);
+    // ?? getAvailableTemplates(); TODO
+    // bool loadFromFile(const std::string&);
+    // bool updateTemplatePose();
   };
 }
 
--- affordance_template_server/src/server.cpp
+++ affordance_template_server/src/server.cpp
@@ -12,6 +12,76 @@ AffordanceTemplateServer::AffordanceTemplateServer()
 AffordanceTemplateServer::~AffordanceTemplateServer() {}
 
 
+std::string AffordanceTemplateServer::getPackagePath(const std::string &pkg_name)
+{
+    ROS_INFO("[AffordanceTemplateServer::getPackagePath]  finding path for package %s", pkg_name.c_str());
+    std::string path = ros::package::getPath(pkg_name); // will be empty if not found
+    if (path.empty())
+        ROS_WARN("[AffordanceTemplateServer::getPackagePath] couldn't find path to package %s!!", pkg_name.c_str());
+    else
+        ROS_INFO("[AffordanceTemplateServer::getPackagePath] found path %s", path.c_str());
+    return path;
+}
+
+std::map<std::string, affordance_template_markers::RobotInterface&> AffordanceTemplateServer::getAvailableRobots(const std::string &path)
+{
+    ROS_INFO("[AffordanceTemplateServer::getAvailableRobots] loading all robots from path %s", path.c_str());
+
+    std::map<std::string, affordance_template_markers::RobotInterface&> robots;
+
+    std::string root = ros::package::getPath(path);
+    if (!root.empty())
+    {
+        if (!boost::filesystem::exists(root) || !boost::filesystem::is_directory(root))
+        {
+            ROS_WARN("[AffordanceTemplateServer::getAvailableRobots] cannot find robots in path %s!!", root.c_str());
+            return robots;
+        }
+
+        // use boost to get all of the files with .yaml extension
+        std::vector<std::string> robot_paths_vec;
+        boost::filesystem::recursive_directory_iterator dir_it(root);
+        boost::filesystem::recursive_directory_iterator end_it;
+        while (dir_it != end_it)
+        {
+            if (boost::filesystem::is_regular_file(*dir_it) && dir_it->path().extension() == ".yaml")
+            {
+                ROS_INFO("[AffordanceTemplateServer::getAvailableRobots] found robot yaml path %s", dir_it->path().string().c_str());
+                robot_paths_vec.push_back(dir_it->path().string());
+            }
+            ++dir_it;
+        }
+
+        // make robot instances with the .yamls we just found
+        for (auto r : robot_paths_vec)
+        {
+            affordance_template_markers::RobotInterface ri;
+            bool loaded = ri.load(r);
+
+            if (!loaded)
+            {
+                ROS_WARN("[AffordanceTemplateServer::getAvailableRobots] robot %s NOT loaded, ignoring.", r.c_str());
+                continue;
+            }
+
+            affordance_template_msgs::RobotConfig rconf = ri.getRobotConfig();
+            if (getPackagePath(rconf.config_package).empty())
+            {
+                ROS_WARN("[AffordanceTemplateServer::getAvailableRobots] config package %s NOT found, ignoring.", r.c_str());
+            }
+            else
+            {
+                ROS_INFO("[AffordanceTemplateServer::getAvailableRobots] config package %s found", r.c_str());
+                // robots[r] = ri;
+            }
+        }
+    }
+    else
+        ROS_WARN("[AffordanceTemplateServer::getAvailableRobots] cannot find robots path!!");
+
+    return robots;
+}
+
 // tester main
 int main(int argc, char **argv)
 {
