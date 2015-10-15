f6eb710ec259d7c9bc15d86f02182c0509ee6989
Seth G <seth@traclabs.com>
2015-10-15 11:35:06 -0500

    reading in ee trajectories now

--- affordance_template_server/src/server.cpp
+++ affordance_template_server/src/server.cpp
@@ -91,6 +91,65 @@ void AffordanceTemplateServer::getAvailableTemplates()
                 at.filename = t.second;
                 ROS_WARN_STREAM("filename is "<<at.filename);
 
+                const rapidjson::Value& traj = d["end_effector_trajectory"];
+                for (rapidjson::SizeType t = 0; t < traj.Size(); ++t)
+                {
+                    affordance_template_object::EndEffector ee;
+                    ee.name = traj[t]["name"].GetString();
+                    const rapidjson::Value& ee_group = traj[t]["end_effector_group"];
+                    ee.id = ee_group[0]["id"].GetInt();
+                    ROS_WARN_STREAM("    found EE trajectory with name: "<<ee.name<<" and id: "<<ee.id);
+
+                    const rapidjson::Value& waypoints = ee_group[0]["end_effector_waypoint"];
+                    for (rapidjson::SizeType w = 0; w < waypoints.Size(); ++w)
+                    {
+                        // find the origin
+                        affordance_template_object::Origin org;
+                        const rapidjson::Value& pos = waypoints[w]["origin"]["xyz"];
+                        org.position[0] = pos[0].GetDouble();
+                        org.position[1] = pos[1].GetDouble();
+                        org.position[2] = pos[2].GetDouble();
+                        const rapidjson::Value& euler = waypoints[w]["origin"]["rpy"];
+                        org.orientation[0] = euler[0].GetDouble();
+                        org.orientation[1] = euler[1].GetDouble();
+                        org.orientation[2] = euler[2].GetDouble();
+
+                        // create the controls
+                        // ** note: it's a little cryptic but basically some of the jsons have "1" for true under controls
+                        // **       so this checks to see if it's bool or int, then converts int if not bool
+                        affordance_template_object::Control ctrl;
+                        const rapidjson::Value& trans = waypoints[w]["controls"]["xyz"];
+                        ctrl.translation[0] = trans[0].IsBool() ? trans[0].GetBool() : (trans[0].GetInt() == 1 ? true : false);
+                        ctrl.translation[1] = trans[1].IsBool() ? trans[1].GetBool() : (trans[1].GetInt() == 1 ? true : false);
+                        ctrl.translation[2] = trans[2].IsBool() ? trans[2].GetBool() : (trans[2].GetInt() == 1 ? true : false);
+                        const rapidjson::Value& rot = waypoints[w]["controls"]["rpy"];
+                        ctrl.rotation[0] = rot[0].IsBool() ? rot[0].GetBool() : (rot[0].GetInt() == 1 ? true : false);
+                        ctrl.rotation[1] = rot[1].IsBool() ? rot[1].GetBool() : (rot[1].GetInt() == 1 ? true : false);
+                        ctrl.rotation[2] = rot[2].IsBool() ? rot[2].GetBool() : (rot[2].GetInt() == 1 ? true : false);
+                        ctrl.scale = waypoints[w]["controls"]["scale"].GetDouble();
+
+                        // fill out the waypoint
+                        affordance_template_object::Waypoint wp;
+                        wp.ee_pose = waypoints[w]["ee_pose"].GetInt();
+                        wp.display_object = waypoints[w]["display_object"].GetString();
+                        wp.origin = org;
+                        wp.controls = ctrl;
+
+                        ee.waypoints.push_back(wp);
+                        
+                        ROS_WARN_STREAM("\twaypoint "<<w+1<<" has ee_pose: "<<wp.ee_pose<<" and display_object: "<<wp.display_object);
+                        ROS_WARN("\t\tat origin XYZ: %g %g %g and RPY: %g %g %g", org.position[0], org.position[1], org.position[2], org.orientation[0], org.orientation[1], org.orientation[2]);
+                        ROS_WARN("\t\tcontrol for axes set to: XYZ: %s %s %s", ctrl.toBoolString(ctrl.translation[0]).c_str(), ctrl.toBoolString(ctrl.translation[1]).c_str(), ctrl.toBoolString(ctrl.translation[2]).c_str());
+                        ROS_WARN("\t\tcontrol for axes set to: RPY: %s %s %s", ctrl.toBoolString(ctrl.rotation[0]).c_str(), ctrl.toBoolString(ctrl.rotation[1]).c_str(), ctrl.toBoolString(ctrl.rotation[2]).c_str());
+                    }
+
+                    at.ee_trajectories.push_back(ee);
+                }
+
+                // TODO display objects from json
+
+                std::cout<<std::endl;
+
                 at_collection_[at.name] = at;
             }
             std::fclose(f_pnt);
