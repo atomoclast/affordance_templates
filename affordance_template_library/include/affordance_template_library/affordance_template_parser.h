95264285d26350e625b8fc516228f7d9dd296e9f
Stephen Hart <swhart@traclabs.com>
2015-10-16 15:35:48 -0500

    oops

---
+++ affordance_template_library/include/affordance_template_library/affordance_template_parser.h
@@ -0,0 +1,33 @@
+#ifndef _AT_PARSER_H_
+#define _AT_PARSER_H_
+
+#include <ros/ros.h>
+
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
+
+#include <rapidjson/document.h>
+#include <rapidjson/filereadstream.h>
+
+#include <affordance_template_library/affordance_template_structure.h>
+#include <affordance_template_library/affordance_template_marker.h>
+#include <affordance_template_library/affordance_template_parser.h>
+
+using namespace rapidjson;
+
+namespace affordance_template_object
+{
+    class AffordanceTemplateParser
+    {
+
+    public: 
+			AffordanceTemplateParser();
+			~AffordanceTemplateParser();
+
+			bool loadFromFile(std::string filename, AffordanceTemplateStructure &at);
+
+    };
+}
+
+#endif
