8533309bf2ea9a6340b6e83701ed7665be5a9314
Stephen Hart <swhart@traclabs.com>
2015-10-16 13:51:09 -0500

    added start of AT class

---
+++ affordance_template_markers/include/affordance_template_markers/affordance_template.h
@@ -0,0 +1,72 @@
+#ifndef _AFFORDANCE_TEMPLATE_H_
+#define _AFFORDANCE_TEMPLATE_H_
+
+#include <ros/ros.h>
+#include <ros/package.h>
+
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+
+#include <interactive_markers/interactive_marker_server.h>
+#include <interactive_markers/menu_handler.h>
+
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/Pose.h>
+
+#include <affordance_template_markers/robot_interface.h>
+
+#include <affordance_template_library/affordance_template_structure.h>
+
+// #include <affordance_template_msgs/RobotConfig.h>
+// #include <affordance_template_msgs/EndEffectorConfig.h>
+// #include <affordance_template_msgs/EndEffectorPoseData.h>
+
+namespace affordance_template 
+{
+  class AffordanceTemplate
+  {
+
+  public:
+    
+    AffordanceTemplate(const ros::NodeHandle nh, 
+                       boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server, 
+                       std::string robot_name, 
+                       std::string template_type);
+    ~AffordanceTemplate();
+
+    void spin();
+
+    void setRobotInterface(boost::shared_ptr<affordance_template_markers::RobotInterface> robot_interface);
+    void setupMenuOptions();
+
+    bool loadFromFile(std::string filename, geometry_msgs::Pose pose, affordance_template_object::AffordanceTemplateStructure &structure);
+
+
+  private:
+
+    // menu config will hold the menu text as well as bool for whether it should have a checkbox
+    typedef std::pair<std::string, bool> MenuConfig;
+
+    // this will handle menus. first item is group name, vector list is nested menu text(s)
+    typedef std::map<std::string, std::vector<std::string> > MenuHandleKey;
+
+    ros::NodeHandle nh_;
+    tf::TransformListener tf_listener_;
+    tf::TransformBroadcaster tf_broadcaster_;
+
+    boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server_;
+    boost::shared_ptr<affordance_template_markers::RobotInterface> robot_interface_;
+
+    std::string robot_name_;
+    std::string template_type_;
+    double loop_rate_;
+
+    std::vector<MenuConfig> object_menu_options_;
+    std::vector<MenuConfig> waypoint_menu_options_;
+      
+
+  };
+}
+
+#endif
---
+++ affordance_template_markers/src/affordance_template.cpp
@@ -0,0 +1,111 @@
+#include <affordance_template_markers/affordance_template.h>
+
+using namespace affordance_template;
+using namespace affordance_template_object;
+using namespace affordance_template_markers;
+
+AffordanceTemplate::AffordanceTemplate(const ros::NodeHandle nh, 
+                                        boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server,  
+                                        std::string robot_name, 
+                                        std::string template_type) :
+  nh_(nh),
+  server_(server),
+  robot_name_(robot_name),
+  template_type_(template_type),
+  loop_rate_(10.0)
+{
+
+  setupMenuOptions();
+
+  ROS_INFO("AffordanceTemplate::init() -- Done Creating new Empty AffordanceTemplate");
+
+}
+
+
+AffordanceTemplate::~AffordanceTemplate() 
+{
+	
+}
+
+void AffordanceTemplate::spin()
+{
+
+  ros::AsyncSpinner spinner(1.0/loop_rate_);
+  spinner.start();
+
+  ROS_INFO("%s started.", nh_.getNamespace().c_str());  
+  ROS_INFO("%s spinning.", nh_.getNamespace().c_str());
+  ros::Rate loop_rate(loop_rate_);
+  while(ros::ok())
+  {
+    loop_rate.sleep();
+  }
+  
+}
+
+void AffordanceTemplate::setRobotInterface(boost::shared_ptr<affordance_template_markers::RobotInterface> robot_interface)
+{
+  robot_interface_ = robot_interface;
+}
+
+
+void AffordanceTemplate::setupMenuOptions() 
+{
+
+  waypoint_menu_options_.clear();
+  waypoint_menu_options_.push_back(MenuConfig("Change End-Effector Pose", false));
+  waypoint_menu_options_.push_back(MenuConfig("Hide Controls", true));
+  waypoint_menu_options_.push_back(MenuConfig("Add Waypoint Before", false));
+  waypoint_menu_options_.push_back(MenuConfig("Add Waypoint After", false));
+  waypoint_menu_options_.push_back(MenuConfig("Delete Waypoint", false));
+  waypoint_menu_options_.push_back(MenuConfig("Move Forward", false));
+  waypoint_menu_options_.push_back(MenuConfig("Move Back", false));
+
+  object_menu_options_.clear();
+  object_menu_options_.push_back(MenuConfig("Add Waypoint Before", false));
+  object_menu_options_.push_back(MenuConfig("Add Waypoint After", false));
+  object_menu_options_.push_back(MenuConfig("Reset", false));
+  object_menu_options_.push_back(MenuConfig("Save", false));
+  object_menu_options_.push_back(MenuConfig("Hide Controls", true));
+  object_menu_options_.push_back(MenuConfig("Choose Trajectory", false));
+
+}
+
+bool AffordanceTemplate::loadFromFile(std::string filename, geometry_msgs::Pose pose, AffordanceTemplateStructure &structure)
+{
+  // atf = open(filename).read()
+  // self.structure = json.loads(atf)
+  // self.structure = self.append_id_to_structure(self.structure)
+  // self.load_initial_parameters(pose)
+  // self.create_from_parameters()
+  // stuff = filename.split("/")
+  // self.filename = stuff[len(stuff)-1]
+  // return self.structure
+  return true;
+}
+
+
+int main(int argc, char **argv)
+{
+ 
+  ros::init(argc, argv, "affordance_template_test");
+  ros::NodeHandle nh("~");
+ 
+  std::string robot_name, template_type;
+  nh.getParam("robot_name", robot_name); 
+  nh.getParam("template_type", template_type); 
+
+  boost::shared_ptr<affordance_template_markers::RobotInterface> robot_interface;
+  robot_interface->load("/home/swhart/ros/catkin_workspace/src/affordance_templates/affordance_template_library/robots/r2.yaml");
+
+  boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server;
+  server.reset( new interactive_markers::InteractiveMarkerServer(std::string(robot_name + "_affordance_template_server"),"",false) );
+
+  AffordanceTemplate at(nh, server, robot_name, template_type);
+  at.setRobotInterface(robot_interface);
+  at.spin();
+ 
+  return 0;
+}
+
+
