f4f755efba0abfa3207245825c6ced80c33ad32e
Seth G <seth@traclabs.com>
2015-10-16 13:54:47 -0500

    adding service interface class

---
+++ affordance_template_server/include/affordance_template_server/service_interface.h
@@ -0,0 +1,16 @@
+#ifndef _SERVICE_INTERFACE_H_
+#define _SERVICE_INTERFACE_H_
+
+#include  <ros/ros.h>
+
+namespace affordance_template_server 
+{
+    class ServiceInterface
+    {
+    public:
+        ServiceInterface();
+        ~ServiceInterface();
+    };
+}
+
+#endif
---
+++ affordance_template_server/src/service_interface.cpp
@@ -0,0 +1,1 @@
+#include <affordance_template_server/service_interface.h>
