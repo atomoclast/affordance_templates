ee2db3b3b9f9a7ab5ebd0ae71bec7d5357b24285
Seth G <seth@traclabs.com>
2015-10-19 12:25:30 -0500

    srvs set up in constructor

--- affordance_template_server/src/interface.cpp
+++ affordance_template_server/src/interface.cpp
@@ -8,107 +8,107 @@ AffordanceTemplateInterface::AffordanceTemplateInterface()
     ros::NodeHandle nh;
 
     const std::string base_srv = "/affordance_template_server/";
-    at_srv_map_["get_robots"]              = nh.advertiseServer("get_robots", &AffordanceTemplateInterface::handleRobotRequest, this);
-    at_srv_map_["get_templates"]           = nh.advertiseServer("get_templates", &AffordanceTemplateInterface::handleTemplateRequest, this);
-    at_srv_map_["load_robot"]              = nh.advertiseServer("load_robot", &AffordanceTemplateInterface::handleLoadRobot, this);
-    at_srv_map_["add_template"]            = nh.advertiseServer("add_template", &AffordanceTemplateInterface::handleAddTemplate, this);
-    at_srv_map_["delete_template"]         = nh.advertiseServer("delete_template", &AffordanceTemplateInterface::handleDeleteTemplate, this);
-    at_srv_map_["get_running"]             = nh.advertiseServer("get_running", &AffordanceTemplateInterface::handleRunning, this);
-    at_srv_map_["plan_command"]            = nh.advertiseServer("plan_command", &AffordanceTemplateInterface::handlePlanCommand, this);
-    at_srv_map_["execute_command"]         = nh.advertiseServer("execute_command", &AffordanceTemplateInterface::handleExecuteCommand, this);
-    at_srv_map_["save_template"]           = nh.advertiseServer("save_template", &AffordanceTemplateInterface::handleSaveTemplate, this);
-    at_srv_map_["add_trajectory"]          = nh.advertiseServer("add_trajectory", &AffordanceTemplateInterface::handleAddTrajectory, this);
-    at_srv_map_["scale_object"]            = nh.advertiseServer("scale_object", &AffordanceTemplateInterface::handleObjectScale, this);
-    at_srv_map_["get_template_status"]     = nh.advertiseServer("get_template_status", &AffordanceTemplateInterface::handleTemplateStatus, this);
-    at_srv_map_["get_status"]              = nh.advertiseServer("get_status", &AffordanceTemplateInterface::handleServerStatus, this);
-    at_srv_map_["set_template_trajectory"] = nh.advertiseServer("set_template_trajectory", &AffordanceTemplateInterface::handleSetTrajectory, this);
-    at_srv_map_["set_template_pose"]       = nh.advertiseServer("set_template_pose", &AffordanceTemplateInterface::handleSetPose, this);
+    at_srv_map_["get_robots"]              = nh.advertiseService("get_robots", &AffordanceTemplateInterface::handleRobotRequest, this);
+    at_srv_map_["get_templates"]           = nh.advertiseService("get_templates", &AffordanceTemplateInterface::handleTemplateRequest, this);
+    at_srv_map_["load_robot"]              = nh.advertiseService("load_robot", &AffordanceTemplateInterface::handleLoadRobot, this);
+    at_srv_map_["add_template"]            = nh.advertiseService("add_template", &AffordanceTemplateInterface::handleAddTemplate, this);
+    at_srv_map_["delete_template"]         = nh.advertiseService("delete_template", &AffordanceTemplateInterface::handleDeleteTemplate, this);
+    at_srv_map_["get_running"]             = nh.advertiseService("get_running", &AffordanceTemplateInterface::handleRunning, this);
+    at_srv_map_["plan_command"]            = nh.advertiseService("plan_command", &AffordanceTemplateInterface::handlePlanCommand, this);
+    at_srv_map_["execute_command"]         = nh.advertiseService("execute_command", &AffordanceTemplateInterface::handleExecuteCommand, this);
+    at_srv_map_["save_template"]           = nh.advertiseService("save_template", &AffordanceTemplateInterface::handleSaveTemplate, this);
+    at_srv_map_["add_trajectory"]          = nh.advertiseService("add_trajectory", &AffordanceTemplateInterface::handleAddTrajectory, this);
+    at_srv_map_["scale_object"]            = nh.advertiseService("scale_object", &AffordanceTemplateInterface::handleObjectScale, this);
+    at_srv_map_["get_template_status"]     = nh.advertiseService("get_template_status", &AffordanceTemplateInterface::handleTemplateStatus, this);
+    at_srv_map_["get_status"]              = nh.advertiseService("get_status", &AffordanceTemplateInterface::handleServerStatus, this);
+    at_srv_map_["set_template_trajectory"] = nh.advertiseService("set_template_trajectory", &AffordanceTemplateInterface::handleSetTrajectory, this);
+    at_srv_map_["set_template_pose"]       = nh.advertiseService("set_template_pose", &AffordanceTemplateInterface::handleSetPose, this);
 }
 
 AffordanceTemplateInterface::~AffordanceTemplateInterface(){}
 
-bool handleRobotRequest(GetRobotConfigInfo::Request &req, GetRobotConfigInfo::Response &res)
+bool AffordanceTemplateInterface::handleRobotRequest(GetRobotConfigInfo::Request &req, GetRobotConfigInfo::Response &res)
 {
     return true;
 }
 
-bool handleTemplateRequest(GetAffordanceTemplateConfigInfo::Request &req, GetAffordanceTemplateConfigInfo::Response &res)
+bool AffordanceTemplateInterface::handleTemplateRequest(GetAffordanceTemplateConfigInfo::Request &req, GetAffordanceTemplateConfigInfo::Response &res)
 {
     return true;
 }
 
-bool handleLoadRobot(LoadRobotConfig::Request &req, LoadRobotConfig::Response &res)
+bool AffordanceTemplateInterface::handleLoadRobot(LoadRobotConfig::Request &req, LoadRobotConfig::Response &res)
 {
     return true;
 }
 
-bool handleAddTemplate(AddAffordanceTemplate::Request &req, AddAffordanceTemplate::Response &res)
+bool AffordanceTemplateInterface::handleAddTemplate(AddAffordanceTemplate::Request &req, AddAffordanceTemplate::Response &res)
 {
     return true;
 }
 
-bool handleDeleteTemplate(DeleteAffordanceTemplate::Request &req, DeleteAffordanceTemplate::Response &res)
+bool AffordanceTemplateInterface::handleDeleteTemplate(DeleteAffordanceTemplate::Request &req, DeleteAffordanceTemplate::Response &res)
 {
     return true;
 }
 
-bool handleRunning(GetRunningAffordanceTemplates::Request &req, GetRunningAffordanceTemplates::Response &res)
+bool AffordanceTemplateInterface::handleRunning(GetRunningAffordanceTemplates::Request &req, GetRunningAffordanceTemplates::Response &res)
 {
     return true;
 }
 
-bool handlePlanCommand(AffordanceTemplatePlanCommand::Request &req, AffordanceTemplatePlanCommand::Response &res)
+bool AffordanceTemplateInterface::handlePlanCommand(AffordanceTemplatePlanCommand::Request &req, AffordanceTemplatePlanCommand::Response &res)
 {
     return true;
 }
 
-bool handleExecuteCommand(AffordanceTemplateExecuteCommand::Request &req, AffordanceTemplateExecuteCommand::Response &res)
+bool AffordanceTemplateInterface::handleExecuteCommand(AffordanceTemplateExecuteCommand::Request &req, AffordanceTemplateExecuteCommand::Response &res)
 {
     return true;
 }
 
-bool handleSaveTemplate(SaveAffordanceTemplate::Request &req, SaveAffordanceTemplate::Response &res)
+bool AffordanceTemplateInterface::handleSaveTemplate(SaveAffordanceTemplate::Request &req, SaveAffordanceTemplate::Response &res)
 {
     return true;
 }
 
-bool handleAddTrajectory(AddAffordanceTemplateTrajectory::Request &req, AddAffordanceTemplateTrajectory::Response &res)
+bool AffordanceTemplateInterface::handleAddTrajectory(AddAffordanceTemplateTrajectory::Request &req, AddAffordanceTemplateTrajectory::Response &res)
 {
     return true;
 }
 
-bool handleObjectScale(ScaleDisplayObject::Request &req, ScaleDisplayObject::Response &res)
+bool AffordanceTemplateInterface::handleObjectScale(ScaleDisplayObject::Request &req, ScaleDisplayObject::Response &res)
 {
     return true;
 }
 
-bool handleTemplateStatus(GetAffordanceTemplateStatus::Request &req, GetAffordanceTemplateStatus::Response &res)
+bool AffordanceTemplateInterface::handleTemplateStatus(GetAffordanceTemplateStatus::Request &req, GetAffordanceTemplateStatus::Response &res)
 {
     return true;
 }
 
-bool handleServerStatus(GetAffordanceTemplateServerStatus::Request &req, GetAffordanceTemplateServerStatus::Response &res)
+bool AffordanceTemplateInterface::handleServerStatus(GetAffordanceTemplateServerStatus::Request &req, GetAffordanceTemplateServerStatus::Response &res)
 {
     return true;
 }
 
-bool handleSetTrajectory(SetAffordanceTemplateTrajectory::Request &req, SetAffordanceTemplateTrajectory::Response &res)
+bool AffordanceTemplateInterface::handleSetTrajectory(SetAffordanceTemplateTrajectory::Request &req, SetAffordanceTemplateTrajectory::Response &res)
 {
     return true;
 }
 
-bool handleSetPose(SetAffordanceTemplatePose::Request &req, SetAffordanceTemplatePose::Response &res)
+bool AffordanceTemplateInterface::handleSetPose(SetAffordanceTemplatePose::Request &req, SetAffordanceTemplatePose::Response &res)
 {
     return true;
 }
 
 
-bool handleObjectScaleCallback(const ScaleDisplayObjectInfo &data)
+bool AffordanceTemplateInterface::handleObjectScaleCallback(const ScaleDisplayObjectInfo &data)
 {
     return true;
 }
 
-bool getTemplateStatus(AffordanceTemplateStatus &status, std::string template_name, int template_id, std::string traj_name, std::string frame_id="")
+bool AffordanceTemplateInterface::getTemplateStatus(AffordanceTemplateStatus &status, std::string template_name, int template_id, std::string traj_name, std::string frame_id="")
 {
     return true;
 }
