ca8a5e7271219dd5d8e6a154d471a924416a5e22
Seth G <seth@traclabs.com>
2015-10-19 12:00:49 -0500

    structure for all srv handlers there; starting on implementation

--- affordance_template_server/include/affordance_template_server/interface.h
+++ affordance_template_server/include/affordance_template_server/interface.h
@@ -3,16 +3,58 @@
 
 #include <affordance_template_server/server.h>
 
+#include <affordance_template_msgs/GetRobotConfigInfo.h>
+#include <affordance_template_msgs/GetAffordanceTemplateConfigInfo.h>
+#include <affordance_template_msgs/LoadRobotConfig.h>
+#include <affordance_template_msgs/AddAffordanceTemplate.h>
+#include <affordance_template_msgs/DeleteAffordanceTemplate.h>
+#include <affordance_template_msgs/GetRunningAffordanceTemplates.h>
+#include <affordance_template_msgs/AffordanceTemplatePlanCommand.h>
+#include <affordance_template_msgs/AffordanceTemplateExecuteCommand.h>
+#include <affordance_template_msgs/SaveAffordanceTemplate.h>
+#include <affordance_template_msgs/AddAffordanceTemplateTrajectory.h>
+#include <affordance_template_msgs/ScaleDisplayObject.h>
+#include <affordance_template_msgs/ScaleDisplayObjectInfo.h>
+#include <affordance_template_msgs/GetAffordanceTemplateStatus.h>
+#include <affordance_template_msgs/GetAffordanceTemplateServerStatus.h>
+#include <affordance_template_msgs/SetAffordanceTemplateTrajectory.h>
+#include <affordance_template_msgs/SetAffordanceTemplatePose.h>
+
+using namespace affordance_template_msgs;
+
 namespace affordance_template_server
 {
     class AffordanceTemplateInterface
     {
-        AffordanceTemplateServer server_;
+        // srv handlers
+        bool handleRobotRequest(GetRobotConfigInfo::Request&, GetRobotConfigInfo::Response&);
+        bool handleTemplateRequest(GetAffordanceTemplateConfigInfo::Request&, GetAffordanceTemplateConfigInfo::Response&);
+        bool handleLoadRobot(LoadRobotConfig::Request&, LoadRobotConfig::Response&);
+        bool handleAddTemplate(AddAffordanceTemplate::Request&, AddAffordanceTemplate::Response&);
+        bool handleRunning(GetRunningAffordanceTemplates::Request&, GetRunningAffordanceTemplates::Response&);
+        bool handleTemplateKill(DeleteAffordanceTemplate::Request&, DeleteAffordanceTemplate::Response&);
+        bool handlePlanCommand(AffordanceTemplatePlanCommand::Request&, AffordanceTemplatePlanCommand::Response&);
+        bool handleExecuteCommand(AffordanceTemplateExecuteCommand::Request&, AffordanceTemplateExecuteCommand::Response&);
+        bool handleSaveTemplate(SaveAffordanceTemplate::Request&, SaveAffordanceTemplate::Response&);
+        bool handleAddTrajectory(AddAffordanceTemplateTrajectory::Request&, AddAffordanceTemplateTrajectory::Response&);
+        bool handleObjectScale(ScaleDisplayObject::Request&, ScaleDisplayObject::Response&);
+        bool handleTemplateStatusRequest(GetAffordanceTemplateStatus::Request&, GetAffordanceTemplateStatus::Response&);
+        bool handleServerStatusRequest(GetAffordanceTemplateServerStatus::Request&, GetAffordanceTemplateServerStatus::Response&);
+        bool handleSetTrajectory(SetAffordanceTemplateTrajectory::Request&, SetAffordanceTemplateTrajectory::Response&);
+        bool handleSetPose(SetAffordanceTemplatePose::Request&, SetAffordanceTemplatePose::Response&);
+        
+        bool handleObjectScaleCallback(const ScaleDisplayObjectInfo&);
+        bool getTemplateStatus(AffordanceTemplateStatus&, std::string, int, std::string, std::string);
+
+        AffordanceTemplateServer at_server_;
+        tf::TransformListener listener_;
+        std::map<std::string, ros::ServiceServer> at_srv_map_;
 
     public:
         AffordanceTemplateInterface();
         ~AffordanceTemplateInterface();
     };
+        
 }
 
 #endif
--- affordance_template_server/src/interface.cpp
+++ affordance_template_server/src/interface.cpp
@@ -1,6 +1,7 @@
 #include <affordance_template_server/interface.h>
 
 using namespace affordance_template_server;
+using namespace affordance_template_msgs;
 
 AffordanceTemplateInterface::AffordanceTemplateInterface()
 {
@@ -8,3 +9,89 @@ AffordanceTemplateInterface::AffordanceTemplateInterface()
 }
 
 AffordanceTemplateInterface::~AffordanceTemplateInterface(){}
+
+bool handleRobotRequest(GetRobotConfigInfo::Request &req, GetRobotConfigInfo::Response &res)
+{
+    return true;
+}
+
+bool handleTemplateRequest(GetAffordanceTemplateConfigInfo::Request &req, GetAffordanceTemplateConfigInfo::Response &res)
+{
+    return true;
+}
+
+bool handleLoadRobot(LoadRobotConfig::Request &req, LoadRobotConfig::Response &res)
+{
+    return true;
+}
+
+bool handleAddTemplate(AddAffordanceTemplate::Request &req, AddAffordanceTemplate::Response &res)
+{
+    return true;
+}
+
+bool handleRunning(GetRunningAffordanceTemplates::Request &req, GetRunningAffordanceTemplates::Response &res)
+{
+    return true;
+}
+
+bool handleTemplateKill(DeleteAffordanceTemplate::Request &req, DeleteAffordanceTemplate::Response &res)
+{
+    return true;
+}
+
+bool handlePlanCommand(AffordanceTemplatePlanCommand::Request &req, AffordanceTemplatePlanCommand::Response &res)
+{
+    return true;
+}
+
+bool handleExecuteCommand(AffordanceTemplateExecuteCommand::Request &req, AffordanceTemplateExecuteCommand::Response &res)
+{
+    return true;
+}
+
+bool handleSaveTemplate(SaveAffordanceTemplate::Request &req, SaveAffordanceTemplate::Response &res)
+{
+    return true;
+}
+
+bool handleAddTrajectory(AddAffordanceTemplateTrajectory::Request &req, AddAffordanceTemplateTrajectory::Response &res)
+{
+    return true;
+}
+
+bool handleObjectScale(ScaleDisplayObject::Request &req, ScaleDisplayObject::Response &res)
+{
+    return true;
+}
+
+bool handleTemplateStatusRequest(GetAffordanceTemplateStatus::Request &req, GetAffordanceTemplateStatus::Response &res)
+{
+    return true;
+}
+
+bool handleServerStatusRequest(GetAffordanceTemplateServerStatus::Request &req, GetAffordanceTemplateServerStatus::Response &res)
+{
+    return true;
+}
+
+bool handleSetTrajectory(SetAffordanceTemplateTrajectory::Request &req, SetAffordanceTemplateTrajectory::Response &res)
+{
+    return true;
+}
+
+bool handleSetPose(SetAffordanceTemplatePose::Request &req, SetAffordanceTemplatePose::Response &res)
+{
+    return true;
+}
+
+
+bool handleObjectScaleCallback(const ScaleDisplayObjectInfo &data)
+{
+    return true;
+}
+
+bool getTemplateStatus(AffordanceTemplateStatus &status, std::string template_name, int template_id, std::string traj_name, std::string frame_id="")
+{
+    return true;
+}
