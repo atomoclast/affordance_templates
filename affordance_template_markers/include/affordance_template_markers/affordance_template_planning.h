4a1d399ea0a88c91860ff16ba22afd68654efe2a
Seth G <seth@traclabs.com>
2015-11-12 15:48:31 -0600

    added in another constructor that will create the robot interface is one is not provided

--- affordance_template_markers/include/affordance_template_markers/affordance_template_planning.h
+++ affordance_template_markers/include/affordance_template_markers/affordance_template_planning.h
@@ -2,8 +2,14 @@
 #define _AFFORDANCE_TEMPLATE_ACTION_H_
 
 #include <ros/ros.h>
+#include <ros/package.h>
+
+#define BOOST_NO_CXX11_SCOPED_ENUMS
+#include <boost/filesystem.hpp>
+
 #include <actionlib/server/simple_action_server.h>
 #include <affordance_template_msgs/PlanAction.h>
+#include <affordance_template_markers/robot_interface.h>
 
 namespace affordance_template_action
 {
@@ -13,9 +19,11 @@ namespace affordance_template_action
 
         ros::NodeHandle nh_;
         actionlib::SimpleActionServer<affordance_template_msgs::PlanAction> action_server_;
+        boost::shared_ptr<affordance_template_markers::RobotInterface> robot_interface_;
 
     public:
         PlanningServer(const std::string&);
+        PlanningServer(const std::string&, boost::shared_ptr<affordance_template_markers::RobotInterface>);
         ~PlanningServer(){}
     };
 }
