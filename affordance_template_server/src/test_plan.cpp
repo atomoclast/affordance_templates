633499102d9d3ba605fd39dc4ab04b95651ac4ee
Seth G <seth@traclabs.com>
2015-12-03 14:56:53 -0600

    added exe actionlib skeleton to AT

--- affordance_template_server/src/test_plan.cpp
+++ affordance_template_server/src/test_plan.cpp
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
   ROS_INFO("Action server started, sending goal.");
   
   affordance_template_msgs::PlanGoal goal;
-  goal.ee.push_back("left_hand");
+  goal.groups.push_back("left_hand");
   goal.steps = 0; // 0 to plan for all steps
   goal.planning = affordance_template_msgs::PlanGoal::CARTESIAN;
   goal.backwards = false;
